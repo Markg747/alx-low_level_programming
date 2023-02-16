319099d18ecc8425290d2acb7a466a32ca3bea15
Mark Kamau <markgitaka@gmail.com>
2023-02-16 02:28:54 -0800

    sizeof()

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,10 @@
 
 int main(void)
 {
-	printf("%d", sizeof());
+	printf("Size of a char: %d", sizeof(char));
+	printf("Size of an int: %d", sizeof(int));
+	printf("Size of a long int: %d", sizeof(long int));
+	printf("Size of a long long int: %d", sizeof(long long int));
+	printf("Size of a float: %d", sizeof(float));
 	return (0);
 }
