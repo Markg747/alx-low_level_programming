f7a9b0fc767730194e608707f74188799035e18e
Mark Kamau <markgitaka@gmail.com>
2023-02-17 06:10:51 -0800

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Start point
+ *
+ *Description: Prints alphabets
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+		putchar('\n');
+	}
+	return (0);
+}
