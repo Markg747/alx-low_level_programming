77953989041b216197ea2b77e91117aa1d1810cc
Mark Kamau <markgitaka@gmail.com>
2023-02-17 13:37:29 -0800

    Double up

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - start point
+ *
+ *Description: Double up
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 100; x++)
+	{
+		for (y = x + 1; y < 100; y++)
+		{
+			putchar(x / 10 + '0');
+			putchar(x % 10 + '0');
+			putchar(' ');
+			putchar(y / 10 + '0');
+			putchar(y % 10 + '0');
+			if (x == 98 && y == 99)
+			{
+				continue;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
