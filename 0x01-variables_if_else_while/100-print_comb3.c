b052d2ff596d6178726110c6e3fd5eb4a9aae780
Mark Kamau <markgitaka@gmail.com>
2023-02-17 12:41:43 -0800

    Combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,9 +13,9 @@ int main(void)
 	int x;
 	int y;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 0; x < 10; x++) 
 	{
-		for (y = x + 1; y <= 9; y++)
+		for (y = x + 1; y < 10; y++)
 		{
 			putchar(x + '0');
 			putchar(y + '0');
