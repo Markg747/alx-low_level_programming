8a84d72e6cc537c229b70826cb4f403242b7c080
Mark Kamau <markgitaka@gmail.com>
2023-02-17 07:44:37 -0800

    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Start point
+ *
+ *Description: Prints numbers 0 to 9
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
