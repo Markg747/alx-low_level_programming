a1c32f2fab3cf99266420bc9d2708b86fb761d1c
Mark Kamau <markgitaka@gmail.com>
2023-02-21 06:51:53 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_last_digit- Start point
+ *
+ *@x: stores integer
+ *
+ *Return: Always 0
+ */
+
+int print_last_digit(int x)
+{
+	if ((x % 10) > 0)
+	{
+		_putchar((x % 10) + '0');
+	}
+	else if ((x % 10) < 0)
+	{
+		_putchar(-(x % 10) + '0');
+	}
+}
