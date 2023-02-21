be9518d3350d4301e2c429ec93143c653fae5685
Mark Kamau <markgitaka@gmail.com>
2023-02-21 05:13:36 -0800

    if c is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_isalpha - Start point
+ *
+ *@c: check is it is an alphabetic character
+ *
+ *Return: Always 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c >= 'A' && c <= 'z' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
