b13daf9f8f2ad93becb86452fbce7739c4b84fe5
Mark Kamau <markgitaka@gmail.com>
2023-02-21 02:45:29 -0800

    Lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ *main - check code
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
