12bff60854f0ffeb578d953a03964d791ca61618
Mark Kamau <markgitaka@gmail.com>
2023-02-21 10:52:28 -0800

    sum of numbers under 1024 divisible by 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,14 +9,15 @@
 int main(void)
 {
 	int x;
+	int sum;
 
 	for (x = 0; x < 1024; x++)
 	{
 		if (((x % 3) == 0) || ((x % 5) == 0))
 		{
-			printf("%d, ", x);
+			sum = sum + x;
 		}
 	}
-	printf("\n");
+	printf("%d\n", sum);
 	return (0);
 }
