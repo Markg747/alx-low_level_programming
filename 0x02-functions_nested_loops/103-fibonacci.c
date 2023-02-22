70931a002d660c7df7bde9de8904b57360be9489
Mark Kamau <markgitaka@gmail.com>
2023-02-22 07:57:17 -0800

    Add fib

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,15 +13,13 @@ int main(void)
 	long sum = fib1;
 	int i = 0;
 
-	while ((fib0 + fib1) < 4000000)
+	while (fib0 + fib1 < 4000000)
 	{
-		fib1 = fib1 + fib0;
-		if ((fib1 % 2) == 0)
-		{
-			sum = sum + fib1;
+		fib1 += fib0;
+		if (fib1 % 2 == 0)
+			sum  += fib1;
 			fib0 = fib1 - fib0;
 			i++;
-		}
 
 	}
 	printf("%ld", sum);
