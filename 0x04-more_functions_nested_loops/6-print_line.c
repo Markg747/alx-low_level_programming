ce660faebf290a8c910bc067d9c64c1bd2c09f7b
Mark Kamau <markgitaka@gmail.com>
2023-02-23 03:57:12 -0800

    Line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_line- prints line
+ *@n: variable
+ *Return: 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
