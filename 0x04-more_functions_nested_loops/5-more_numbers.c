23be1fc90dee866091023889247fb97a8216aea9
Mark Kamau <markgitaka@gmail.com>
2023-02-23 02:17:15 -0800

    0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *more_numbers- prints 0 to 14 ten times
+ *
+ *Return: 0
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b > 9)
+			{
+				_putchar((b / 10) + 48);
+				_putchar((b % 10) + 48);
+			}
+			else
+			{
+				_putchar(b + 48);
+			}
+		}
+	}
+}
