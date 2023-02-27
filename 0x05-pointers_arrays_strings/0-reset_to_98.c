64d4ef596e49a01670ceb501d4a45c780a53a6c9
Mark Kamau <markgitaka@gmail.com>
2023-02-27 01:05:10 -0800

    Reset value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - resets number to 98
+ *
+ *@*n: variable ptr
+ *
+ *Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef PUTCHAR_H_
+#define PUTCHAR_H_
+
+#include <stdio.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+#endif
