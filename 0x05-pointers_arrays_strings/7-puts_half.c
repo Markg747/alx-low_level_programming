48d11412327839968e9d555f5194433f9b89a220
Mark Kamau <markgitaka@gmail.com>
2023-02-27 05:29:57 -0800

    Half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *puts_half - prints half the string
+ *
+ *@str: string variable
+ *
+ *Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int len;
+	int n;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len + 1) / 2;
+	}
+	for (i = n; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
