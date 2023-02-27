927c2cd4f048116c626478795b82064b35cd2336
Mark Kamau <markgitaka@gmail.com>
2023-02-27 04:54:58 -0800

    Every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *puts2 - prints out every other character in a string
+ *
+ *@str: string variable
+ *
+ *Return: 0
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+	int i = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	len = len - 1;
+
+	for (; i <= len; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
