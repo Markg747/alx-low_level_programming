9c32ea370d3b6d7add6b89dc37e65bdc13cb3d44
Mark Kamau <markgitaka@gmail.com>
2023-02-27 08:08:46 -0800

    Rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,10 +18,10 @@ void rev_string(char *s)
 	{
 		len++;
 	}
-	while (i < len--)
+	for (i = len - 1; i >= len / 2; i--)
 	{
 		tmp = s[i];
-		s[i++] = s[len];
-		s[len] = tmp;
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
 	}
 }
