127f1d523584547477410848d815b9dcb4e96efb
Mark Kamau <markgitaka@gmail.com>
2023-03-01 07:50:20 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,14 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n && dest[i] != '\0'; i++)
+	while (dest[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
 		dest[i] = '\0';
 	}
