db8a5e8ae8162b28a17fff8fd3f947d28fc43e34
Mark Kamau <markgitaka@gmail.com>
2023-03-01 10:23:34 -0800

    Lower to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ **string_toupper - changes lowercase to uppercase
+ *
+ *@c: string input
+ *
+ *Return: c
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] >= 97 && c[i] <= 122)
+		{
+			c[i] = c[i] - 32;
+		}
+	}
+	return (c);
+}
