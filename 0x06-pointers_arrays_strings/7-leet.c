67a19de15a9e61e6d80656086ad2c7414c4b6d40
Mark Kamau <markgitaka@gmail.com>
2023-03-03 07:13:49 -0800

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,9 +12,9 @@ char *leet(char *n)
 {
 	int i;
 	int j;
-	char x[20] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L', '\0'};
+	char x[11] = "aAeEoOtTlL";
 
-	int y[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
+	char y[11] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
