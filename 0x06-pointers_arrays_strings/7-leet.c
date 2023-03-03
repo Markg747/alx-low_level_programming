0f5a8e3a9ab269897b671bec7903211cf816e2fb
Mark Kamau <markgitaka@gmail.com>
2023-03-03 06:52:01 -0800

    Leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ **leet - encodes leetspeak
+ *
+ *@n: string to be encoded
+ *
+ *Return: n
+ */
+
+char *leet(char *n)
+{
+	int i;
+	int j;
+	char x[11] = "aAeEoOtTlL";
+	int y[10] = {4,4,3,3,0,0,7,7,1,1};
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == x[j])
+			{
+				n[i] = y[j];
+			}
+		}
+	}
+	return (n);
+}
