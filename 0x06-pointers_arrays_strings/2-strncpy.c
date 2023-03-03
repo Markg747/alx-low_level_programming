8b5436276955e43f2c55b8ce4448a02cd4b4a8b3
Mark Kamau <markgitaka@gmail.com>
2023-03-03 03:07:43 -0800

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,26 +15,25 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 	int j;
-	char *tmp = dest;
 
 	i = 0;
 
-	while (src[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
 	j = 0;
 
-	while (dest[j] != '\0' && j < n)
+	while (src[j] != '\0' && j < n)
 	{
-		dest[j] = src[i];
+		dest[i] = src[j];
 		j++;
 	}
 	while (j < n)
 	{
-		dest[j] = '\0';
+		dest[i] = '\0';
 		j++;
 	}
-	return (tmp);
+	return (dest);
 }
