c670dda7aaca080f7f5e46cc6ade07295629021e
Mark Kamau <markgitaka@gmail.com>
2023-03-08 10:20:38 -0800

    squareroot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ *square - gets squareroot of number
+ *@x: squareoot
+ *@y: square number
+ *
+ *Return: the squareroot
+ */
+
+int square(int x, int y)
+{
+	if (y == 1)
+	{
+		return (1);
+	}
+	else if (x * x == y)
+	{
+		return (x);
+	}
+	else
+	{
+		return (square(x + 1, y));
+	}
+}
+
+/**
+ *_sqrt_recursion - returns the natural square root of a number
+ *
+ *@n: the square number
+ *
+ *Return: the squareroot
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (square(1, n));
+}
