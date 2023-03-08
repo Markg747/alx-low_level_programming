57b08ca445359faedbb5d252c52e5017a7e6b035
Mark Kamau <markgitaka@gmail.com>
2023-03-08 11:18:20 -0800

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -24,3 +24,29 @@ int prime(int a, int divider)
 		return (a, divider + 1);
 	}
 }
+
+/**
+ *is_prime_number - checks if number is prime number
+ *
+ *@n: number to check
+ *
+ *Return: 1 if prime number, 0 if not prime
+ */
+
+int is_prime_number(int n)
+{
+	int divider = 2;
+
+	if (n % divider == 0 || n < 2)
+	{
+		return (0);
+	}
+	else if (n == divider)
+	{
+		return (1);
+	}
+	else
+	{
+		return (prime(n, divider + 1));
+	}
+}
