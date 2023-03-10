d4656db1356a5eeb27ae084196c2f5976ca2272b
Mark Kamau <markgitaka@gmail.com>
2023-03-10 09:21:16 -0800

    Multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - multiplies two numbers
+ *
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum = sum + argv[i]
+		}
+		printf("%d\n", sum);
+		if (argv[i] 
+	}
+	if (argc == 0)
+	{
+		printf("%d", 0);
+	}
