33f99876a6bc4e411f758b98fc1bdff0658262bf
Mark Kamau <markgitaka@gmail.com>
2023-03-13 01:40:19 -0700

    Malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,11 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	ptr = (char *) malloc(size * sizeof(char));
 
 	if (ptr == 0)
