abbf187f6726045cb73d0f9067fcba02ed768853
Mark Kamau <markgitaka@gmail.com>
2023-03-20 11:41:01 -0700

    redefine struct name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,11 +15,6 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 
-struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-};
 #endif
