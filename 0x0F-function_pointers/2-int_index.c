7e421d5d5b99223204a071d84c56bb9822bdf386
Mark Kamau <markgitaka@gmail.com>
2023-03-22 06:57:34 -0700

    search int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,22 +15,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	unsigned int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 	{
 		return (-1);
 	}
-	if (array != NULL && size > 0 && cmp != NULL)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
+		if (cmp(array[i]) != 0)
 		{
-			if (cmp(array[i]) != 0)
-			{
-				return (i);
-			}
-			else
-			{
-				return (-1);
-			}
+			return (i);
+		}
+		else
+		{
+			return (-1);
 		}
 	}
 }
