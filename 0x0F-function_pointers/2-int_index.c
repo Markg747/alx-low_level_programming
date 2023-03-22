86951dc86b8e2c8b5fb8bb902aa9767de2032218
Mark Kamau <markgitaka@gmail.com>
2023-03-22 06:50:50 -0700

    search number

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ *int_index -  searches for an integer
+ *
+ *@array: array to be searched
+ *@size: size of array
+ *@cmp: function pointer
+ *
+*Return: index of the first element
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (size <= 0 && array != NULL && cmp == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	return (0);
+}
