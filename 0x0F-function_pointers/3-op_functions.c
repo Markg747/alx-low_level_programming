04dc21d4ec4dbcf985896841f995b903b71ee58d
Mark Kamau <markgitaka@gmail.com>
2023-03-25 09:34:29 -0700

    Opcodes

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ *op_add - performs addition
+ *
+ *@a: num1
+ *@b: num2
+ *
+ *Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - performs subtraction
+ *
+ *@a: num1
+ *@b: num2
+ *
+ *Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - performs multiplication
+ *
+ *@a: num1
+ *@b: num2
+ *
+ *Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - performs division
+ *
+ *@a: num1
+ *@b: num2
+ *
+ *Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - performs modulus
+ *
+ *@a: num1
+ *@b: num2
+ *
+ *Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
