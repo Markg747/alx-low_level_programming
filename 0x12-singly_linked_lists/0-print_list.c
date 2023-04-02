b77cbaad246d3d96d58897d391330ee51f94b30b
Mark Kamau <markgitaka@gmail.com>
2023-04-02 10:39:37 -0700

    print m=nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *print_list -  prints all the elements of a list_t list.
+ *
+ *@h: first node of list
+ *
+ *Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	const list_t *ptr = h;
+
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+		{
+			printf("[%u] %s\n", h->len, "(nil)");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
