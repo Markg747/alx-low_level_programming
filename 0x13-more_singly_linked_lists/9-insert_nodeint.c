34fdc1408062456c4ab3fc42f0a0a8d2573a7344
Mark Kamau <markgitaka@gmail.com>
2023-04-04 12:44:48 -0700

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr;
 
 	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 	ptr->n = n;
 	ptr->next = NULL;
 
