819ba653506c39c32c3316a739fa1839a6b00a85
Mark Kamau <markgitaka@gmail.com>
2023-04-04 08:30:05 -0700

    new node beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -19,12 +19,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	{
 		return (NULL);
 	}
-	else
-	{
-		ptr = malloc(sizeof(listint_t));
-		ptr->n = n;
-		ptr->next = *head;
-	}
+	ptr = malloc(sizeof(listint_t));
+	ptr->n = n;
+	ptr->next = *head;
+	
 	*head = ptr;
+
 	return (ptr);
 }
