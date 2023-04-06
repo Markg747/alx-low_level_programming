297daf25d51dd7cf567f90c8dc08e76f156613af
Mark Kamau <markgitaka@gmail.com>
2023-04-06 10:15:55 -0700

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *binary_to_uint - converts a binary number to an unsigned int
+ *
+ *@b: pointer to the binary characters
+ *
+ *Return: converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int base = 1, res = 0;
+	int len = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	for (int i = 0; b[i] != '\0'; i++)
+	{
+		len++;
+	}
+	while (len)
+	{
+		if (b[len - 1] != '0' && b[len - 1] != '1')
+		{
+			return (0);
+		}
+		if (b[len - 1] == '1')
+		{
+			res = res + base;
+		}
+		base = base * 2;
+		len--;
+	}
+	return (res);
+}
