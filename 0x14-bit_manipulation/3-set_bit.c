91804ee19998aa8087f583e5a5e2fc4d375c60df
Mark Kamau <markgitaka@gmail.com>
2023-04-07 05:22:51 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,9 +12,12 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
+	{
 		return (-1);
-
-	*n ^= (1 << index);
-
-	return (1);
+	}
+	else
+	{
+		*n ^= (1 << index);
+		return (1);
+	}
 }
